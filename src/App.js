18b4d2b010853411f59623271da4c02452e98dba
Jared Jones <jkjones.dev@gmail.com>
2018-08-29 08:33:17 -0500

    Fixing racecondition with function setState chains

--- src/App.js
+++ src/App.js
@@ -66,83 +66,54 @@ class App extends Component {
     }
 
     onGameEnd = (winningSide) => {
-        let { players, currentPlayers, playerList } = this.state;
-        let winners = currentPlayers[winningSide];
-        let losers = currentPlayers[winningSide === 'left' ? 'right' : 'left'];
-        // Update stats
-        players[winners.player1].wins++;
-        players[winners.player2].wins++;
-        players[winners.player1].games++;
-        players[winners.player2].games++;
-        players[losers.player1].games++;
-        players[losers.player2].games++;
-        // Add losers back to the playerList
-        playerList = [...playerList, losers.player1, losers.player2 ];
-        // Assign new teams
+        this.setState((prevState) => {
+            let { players, currentPlayers, playerList } = prevState;
+            let winners = currentPlayers[winningSide];
+            let losers = currentPlayers[winningSide === 'left' ? 'right' : 'left'];
+            // Update stats
+            players[winners.player1].wins++;
+            players[winners.player2].wins++;
+            players[winners.player1].games++;
+            players[winners.player2].games++;
+            players[losers.player1].games++;
+            players[losers.player2].games++;
+            // Add losers back to the playerList
+            playerList = [...playerList, losers.player1, losers.player2 ];
+            // Assign new teams
+            
+            currentPlayers.left.player1 = winners.player1;
+            currentPlayers.right.player1 = winners.player2;
+            return {
+                players,
+                playerList,
+                currentPlayers
+            };
+        })
         this.findNextPlayer('left', 'player2'),
         this.findNextPlayer('right', 'player2');
-        currentPlayers = { 
-            left: { player1: winners.player1, player2: p1 }, 
-            right: { player1: winners.player2, player2: p2 } 
-        };
-        this.setState({
-            players,
-            playerList: playerListUpdated2,
-            currentPlayers
-        });
-    }
-
-    findNextPlayer2 = (playerList, teammate) => {
-        console.log(playerList);
-        // Use teammate to pick the upcoming player with the furthest play history with teammate
-        const { players } = this.state;
-        let nextPlayer;
-        for(let i in playerList) {
-            let playerInfo = players[playerList[i]];
-            if (!playerInfo.hold) {
-                nextPlayer = playerList.splice(i, 1)[0];
-                break;
-            } else {
-                playerList.push(playerList.splice(i, 1)[0]);
-            }
-        }
-        console.log(playerList);
-        return { nextPlayer, playerList };
     }
 
     findNextPlayer = (side, position) => {
         this.setState((prevState) => {
-            
-        })
-        const { players, playerList, currentPlayers } = this.state;
-        let nextPlayer;
-        for(let i in playerList) {
-            let playerInfo = players[playerList[i]];
-            if (!playerInfo.hold) {
-                nextPlayer = playerList.splice(i, 1)[0];
-                break;
-            } else {
-                playerList.push(playerList.splice(i, 1)[0]);
+            const { players, playerList, currentPlayers } = prevState;
+            for(let i in playerList) {
+                let playerInfo = players[playerList[i]];
+                if (!playerInfo.hold) {
+                    currentPlayers[side][position] = playerList.splice(i, 1)[0];
+                    break;
+                } else {
+                    playerList.push(playerList.splice(i, 1)[0]);
+                }
             }
-        }
+            return { currentPlayers, playerList }
+        });
     }
 
     startGame = () => {
-        const { playerList } = this.state; 
-        let { nextPlayer: p1, playerList: playerListUpdated } = this.findNextPlayer(playerList),
-            { nextPlayer: p2, playerList: playerListUpdated2 } = this.findNextPlayer(playerListUpdated),
-            { nextPlayer: p3, playerList: playerListUpdated3 } = this.findNextPlayer(playerListUpdated2),
-            { nextPlayer: p4, playerList: playerListUpdated4 } = this.findNextPlayer(playerListUpdated3);
-        console.log(`${p1} ${p2} ${p3} ${p4}`)
-        this.setState({
-            currentPlayers: {
-                left: { player1: p1, player2: p3 },
-                right: { player1: p2, player2: p4 }
-            },
-            playerList: playerListUpdated4
-        }, () => {
-            console.log(this.state.currentPlayers);
-        });
+        this.findNextPlayer('left', 'player1'),
+        this.findNextPlayer('right', 'player1'),
+        this.findNextPlayer('left', 'player2'),
+        this.findNextPlayer('right', 'player2');
     }
 
     playerInfoList = () => {
